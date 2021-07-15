e61df04e6114e9bceec29bd1eb0424ad9dc4ecbd
RagavendranMRN <ragavendran1371997@gmail.com>
2021-07-15 21:00:05 +0530

    4 files modified

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../src/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class TaskManager extends Component {
   constructor() {
@@ -17,10 +18,34 @@ export class TaskManager extends Component {
 
   _handleKeyDown = todoValue => {
     let todos = [...this.state.todos];
-    todos.push(todoValue);
+    let todo_obj = {
+      id: this.state.todos.length,
+      title: todoValue,
+      isCompleted: false
+    };
+    todos.push(todo_obj);
+    console.log(todo_obj);
     this.setState({ todos });
   };
 
+  _handleDelete = todoValue => {
+    let todos = [...this.state.todos];
+    todos = todos.filter(todo => todo.id != todoValue);
+    this.setState({ todos });
+  };
+
+  _handleComplete = todoValue => {
+    let todos = [...this.state.todos];
+    todos.map(todo => {
+      if (todo.id == todoValue && todo.isCompleted == false) {
+        todo.isCompleted = true;
+      } else if (todo.id == todoValue && todo.isCompleted == true) {
+        todo.isCompleted = false;
+      }
+    });
+    console.log(todos);
+    this.setState({ todos });
+  };
   // componentDidMount() {
   //   fetch('https://mocki.io/v1/343df756-69ef-4af9-baeb-5b461a97cef8', {
   //     mode: 'cors' // 'cors' by default
@@ -31,7 +56,12 @@ export class TaskManager extends Component {
 
   render() {
     return (
-      <TaskList todos={this.state.todos} onAddTask={this._handleKeyDown} />
+      <TaskList
+        todos={this.state.todos}
+        onAddTask={this._handleKeyDown}
+        onDeleteTask={this._handleDelete}
+        isCompleted={this._handleComplete}
+      />
     );
   }
 }
@@ -39,25 +69,64 @@ export class TaskManager extends Component {
 export function TaskList(props) {
   return (
     <div>
-      <input id="taskData" />
-      <button
-        onClick={() =>
-          props.onAddTask(document.getElementById('taskData').value)
-        }
-      >
-        Add task
-      </button>
+      <div class="input-group mb-3">
+        <input type="text" id="taskData" class="form-control" />
+        <button
+          class="btn btn-primary"
+          type="button"
+          id="button-addon2"
+          onClick={() =>
+            props.onAddTask(document.getElementById('taskData').value)
+          }
+        >
+          Add task
+        </button>
+      </div>
+
       {props.todos.map(todo => (
-        <Task value={todo} />
+        <Task
+          value={todo}
+          onDelete={props.onDeleteTask}
+          isCompleted={props.isCompleted}
+        />
       ))}
     </div>
   );
 }
 
 export function Task(props) {
+  const styles = () => {
+    if (props.value.isCompleted === true) {
+      return { textDecorationLine: 'line-through' };
+    }
+    return;
+  };
+
+  const completedClass = () => {
+    if (props.value.isCompleted === false) {
+      return 'fa fa-check';
+    }
+    return 'fa fa-times';
+  };
   return (
-    <div className="task">
-      <h6>{props.value}</h6>
+    <div className="card m-2">
+      <div className="card-body">
+        <label className="m-1" style={styles()}>
+          {props.value.title}
+        </label>
+        <button
+          className="btn btn-danger trash m-1"
+          onClick={() => props.onDelete(props.value.id)}
+        >
+          <i className="fa fa-trash" />
+        </button>
+        <button
+          className="btn btn-success check m-1"
+          onClick={() => props.isCompleted(props.value.id)}
+        >
+          <i className={completedClass()} />
+        </button>
+      </div>
     </div>
   );
 }
